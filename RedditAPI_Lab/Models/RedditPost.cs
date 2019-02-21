abdcf47241b87210b415be46877f6f48991714de
steve webster <steve.webster109@gmail.com>
2019-02-21 16:54:16 -0500

    initial commit

---
+++ RedditAPI_Lab/Models/RedditPost.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RedditAPI_Lab.Models
+{
+    public class RedditPost
+    {
+        public string Title { get; set; }
+        public string ImageURL { get; set; }
+        public string LinkURL { get; set; }
+        public string data { get; set; }
+    }
+        
+}
